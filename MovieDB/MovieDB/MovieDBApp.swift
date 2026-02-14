19888bb685e43a2ed36aff11bd9fec8407fbc44a
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2026-02-14 21:21:50 +0530

    initial commit

---
+++ MovieDB/MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Rohan Bimal Raj on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
